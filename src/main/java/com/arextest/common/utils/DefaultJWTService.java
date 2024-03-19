c06265fc30e5bfe6ad29d71d2dc1d94f3e50b8bb
Rui Chen <44330697+coryhh@users.noreply.github.com>
2024-03-19 20:38:05 +0800

    feat: modify token and interceptor (#23)

---
+++ src/main/java/com/arextest/common/utils/DefaultJWTService.java
@@ -0,0 +1,75 @@
+package com.arextest.common.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DefaultJWTService {
+
+  private long accessExpireTime;
+  private long refreshExpireTime;
+  private String tokenSecret;
+
+  public String makeAccessToken(String username) {
+    Date date = new Date(System.currentTimeMillis() + accessExpireTime);
+    Algorithm algorithm = Algorithm.HMAC256(tokenSecret);
+    return JWT.create()
+        .withExpiresAt(date)
+        .withClaim("username", username)
+        .sign(algorithm);
+  }
+
+  public String makeRefreshToken(String username) {
+    Date date = new Date(System.currentTimeMillis() + refreshExpireTime);
+    Algorithm algorithm = Algorithm.HMAC256(tokenSecret);
+    return JWT.create()
+        .withExpiresAt(date)
+        .withClaim("username", username)
+        .sign(algorithm);
+
+  }
+
+  public boolean verifyToken(String field) {
+    if (StringUtils.isEmpty(field)) {
+      return false;
+    }
+    return getToken(field) != null;
+  }
+
+  public String getUserName(String token) {
+    if (StringUtils.isEmpty(token)) {
+      return null;
+    }
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(tokenSecret);
+      JWTVerifier build = JWT.require(algorithm).build();
+      DecodedJWT verify = build.verify(token);
+      return verify.getClaim("username").asString();
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+  private DecodedJWT getToken(String token) {
+    if (StringUtils.isEmpty(token)) {
+      return null;
+    }
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(tokenSecret);
+      JWTVerifier build = JWT.require(algorithm).build();
+      return build.verify(token);
+    } catch (Exception e) {
+      return null;
+    }
+  }
+
+}
